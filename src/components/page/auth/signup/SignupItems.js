92469bf02d38a000a765c2becb06a47baa755f96
ss-cosmos-hue <sayo34hashimoto@gmail.com>
2024-03-16 17:42:53 +0900

    add signup page

---
+++ src/components/page/auth/signup/SignupItems.js
@@ -0,0 +1,50 @@
+import authStyles from "@/components/page/auth/auth.module.scss";
+import buttonStyles from "@/styles/button/PillShapedButton.module.scss";
+import ProgressBar from "./ProgressBar";
+import Image from "next/image";
+
+export default function SignupItems() {
+  return (
+    <div className={authStyles.itemsWrapper}>
+      <a href="/home">
+        <Image
+          className={authStyles.backIcon}
+          src="/icons/back.svg"
+          height={30}
+          width={30}
+          alt=""
+        />
+      </a>
+      <h2 className={authStyles.title}>新規登録</h2>
+      <ProgressBar isSignedUp={false} />
+      <div className={authStyles.forms}>
+        <div>
+          <p>メールアドレス</p>
+          <input className={authStyles.textBox}></input>
+          <p>パスワード</p>
+          <input className={authStyles.textBox}></input>
+          <p>パスワード再入力</p>
+          <input className={authStyles.textBox}></input>
+          <p></p>
+          <button className={buttonStyles.pillShapedButton}>次へ</button>
+        </div>
+        <hr className={authStyles.division}></hr>
+        <div>
+          <button className={buttonStyles.pillShapedButtonWhite}>
+            <Image
+              className={authStyles.snsIcon}
+              src="/icons/google.svg"
+              height={25}
+              width={25}
+              alt=""
+            />
+            Googleで新規登録
+          </button>
+        </div>
+        <p className={authStyles.caption}>
+          アカウントをお持ちでない方は<a href="/auth/login">こちら</a>
+        </p>
+      </div>
+    </div>
+  );
+}
