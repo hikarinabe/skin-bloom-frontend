d503b8034a11b1b156b14b84c8e1f1102a7379ad
Gre3nT <fabulous648@gmail.com>
2024-03-16 21:23:34 +0900

    routing

---
+++ src/pages/setting/index.js
@@ -0,0 +1,19 @@
+import {useRouter} from "next/router";
+import { useEffect } from "react";
+
+export default function Setting() {
+    
+    const router = useRouter();
+
+    useEffect(() => {
+        const isLoggedIn = true;
+
+        if(isLoggedIn) {
+            router.push("/setting/profile");
+        } else {
+            router.push("/home/intro");
+        }
+    }, [router]);
+
+    return null;
+}
