4950e50d1bf60c94063b643a0288cde794c11911
Gre3nT <fabulous648@gmail.com>
2024-03-18 21:33:48 +0900

    設定ページの共通要素

--- src/pages/setting/profile/ViewCommonElements.js
+++ src/pages/setting/profile/ViewCommonElements.js
@@ -1,23 +1,33 @@
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./ViewCommonElements.module.scss";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import EditClick from "./EditClick";
+import CancelClick from "./CancelClick";
+import SaveClick from "./SaveClick";
 
-export default function ViewCommonElements(){
+export default function ViewCommonElements() {
     return (
         <div>
             <div className={styles.topColor}></div>
-            <div>
-                <img src="/icons/person.svg"></img>
-                <h1>マイページ</h1>
-             </div>
-            <div class="tabs">
-                <ul>
-                    <li><Link href="./profile">プロフィール</Link></li>
-                    <li><Link href="./password">パスワード</Link></li>
-                    <li><Link href="./record">使用履歴</Link></li>   
-                </ul>
+            <div className="container">
+                <div className={styles.icon}>
+                    <img src="/icons/profile_icon.jpg" className={styles.img}></img>
+                    <p className={styles.title}>マイページ</p>
+                </div>
+                <div>
+                </div>
+                <div className={styles.tabs}>
+                    <div className="tabs w-75">
+                        <ul className="nav nav-underline">
+                            <li className="nav-item"><Link href="./profile" className="nav-link text-primary-emphasis">プロフィール</Link></li>
+                            <li className="nav-item"><Link href="./password" className="nav-link text-primary-emphasis">パスワード</Link></li>
+                            <li className="nav-item"><Link href="./record" className="nav-link text-primary-emphasis">使用履歴</Link></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
-        
+
     );
 }
