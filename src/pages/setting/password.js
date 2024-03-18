78b6cea8e706cccc518582ec56c1791d76694dee
Gre3nT <fabulous648@gmail.com>
2024-03-18 21:34:46 +0900

    パスワード変更画面

--- src/pages/setting/password.js
+++ src/pages/setting/password.js
@@ -1,28 +1,51 @@
+import Image from "next/image";
+import Link from "next/link";
 import Layout from "@/components/Layout/Layout";
 import ViewCommonElements from "./profile/ViewCommonElements";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styles from "./Profile.module.scss";
+import SubmitClick from "./profile/SubmitClick";
+
 
 export default function Password() {
     return (
         <Layout>
+            <ViewCommonElements />
             <main>
-                <ViewCommonElements />
-                <div>
-                    <p>現在のメールアドレス</p>
-                    <p>gmail.com</p>
+                <div className={styles.Button}>
+
+                    <div className="form-check form-check-reverse">
+                        <Link href="./profile" className="btn btn-outline-primary">Cancel</Link>
+                        <button type="button" onClick={SubmitClick} className="btn btn-primary">Submit</button>
+                    </div>
                 </div>
-                <div>
-                    <p>現在のパスワード</p>
-                    <p>*******</p>
-                </div>
-                <div>
-                    <p>新しいパスワード</p>
-                    <p>hoge</p>
-                </div>
-                <div>
-                    <p>新しいパスワード（確認）</p>
-                    <p>hogehoge</p>
+                <div className={styles.contents}>
+                    <div className="w-75">
+                        <div>
+                            <label>現在のパスワード</label>
+                            <div className="input-group">
+                                <span className="input-group-text"><img src="/icons/lock-fill.svg"></img></span>
+                                <input type="password" className="form-control" id="current_password" />
+                            </div>
+                        </div>
+                        <div>
+                            <label>新しいパスワード</label>
+                            <div className="input-group">
+                                <span className="input-group-text"><img src="/icons/lock-fill.svg"></img></span>
+                                <input type="password" className="form-control" id="new_password" />
+                            </div>
+                        </div>
+                        <div>
+                            <label>新しいパスワード（確認）</label>
+                            <div className="input-group">
+                                <span className="input-group-text"><img src="/icons/lock-fill.svg"></img></span>
+                                <input type="password" className="form-control" id="confirm_password" />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </main>
         </Layout>
     );
 }
+
