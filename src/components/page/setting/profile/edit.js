af83371cd17ece2f2d95f57936f0833597ad456d
Namiko <namiko.trym@gmail.com>
2024-03-19 17:22:59 +0900

    save checkの削除

--- src/components/page/setting/profile/edit.js
+++ src/components/page/setting/profile/edit.js
@@ -1,9 +1,29 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Link from "next/link";
 import styles from "../Profile.module.scss";
-import SaveClick from "./SaveClick";
 
 export default function EditProfileItem() {
+  function SaveClick() {
+    const editDom = document.getElementById("editor");
+    // const DOM = document.getElementById("contents");
+    const accountDom = document.getElementById("account");
+    const mailDom = document.getElementById("mail");
+    const birthdayDom = document.getElementById("birthday");
+    const maleDom = document.getElementById("male");
+    const femaleDom = document.getElementById("female");
+    const otherDom = document.getElementById("other");
+    const naDom = document.getElementById("no_answer");
+  
+    console.log(accountDom);
+    console.log(mailDom);
+    console.log(birthdayDom);
+    console.log(maleDom);
+    console.log(femaleDom);
+    console.log(otherDom);
+    console.log(naDom);
+    alert("登録できました");
+  }
+
   return (
     <main>
       <div className={styles.Button}>
