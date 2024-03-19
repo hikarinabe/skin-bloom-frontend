cda31e018474d7259b211d361b2c5ac2669fba69
Namiko <namiko.trym@gmail.com>
2024-03-19 19:08:58 +0900

    run fmt

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,38 +11,38 @@ export default function Header() {
       <div className={styles.rightAligned}>
         <nav className={styles.nav}>
           <ul className={styles.navList}>
-          <li key="Home" className={styles.navItem}>
+            <li key="Home" className={styles.navItem}>
               <Link href="/home/mypage">Home</Link>
-          </li>
-          <li key="Search" className={styles.navItem}>
-            <Link href="/cosmetics/search">Search</Link>
-          </li>
-          <li key="Records" className={styles.navItem}>
-          <Link href="/">Records</Link>
-          </li>
-          {/* todo: あとでAPI接続する */}
-          <li key="Logout" className={styles.navItem}>
-          <Link href="/home/intro">Logout</Link>
-          </li>
+            </li>
+            <li key="Search" className={styles.navItem}>
+              <Link href="/cosmetics/search">Search</Link>
+            </li>
+            <li key="Records" className={styles.navItem}>
+              <Link href="/">Records</Link>
+            </li>
+            {/* todo: あとでAPI接続する */}
+            <li key="Logout" className={styles.navItem}>
+              <Link href="/home/intro">Logout</Link>
+            </li>
           </ul>
         </nav>
         <Link href="">
-        <Image
-          className={styles.icon}
-          alt=""
-          src="/icons/write.svg"
-          width={40}
-          height={40}
-        />
+          <Image
+            className={styles.icon}
+            alt=""
+            src="/icons/write.svg"
+            width={40}
+            height={40}
+          />
         </Link>
         <Link href="/setting/profile">
-        <Image
-          className={styles.icon}
-          alt=""
-          src="/icons/person.svg"
-          width={40}
-          height={40}
-        />
+          <Image
+            className={styles.icon}
+            alt=""
+            src="/icons/person.svg"
+            width={40}
+            height={40}
+          />
         </Link>
       </div>
     </header>
