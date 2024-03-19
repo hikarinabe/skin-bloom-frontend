f6f7392b1636740a4ff4ea17f8c5d0dacbcad8cb
Namiko <namiko.trym@gmail.com>
2024-03-19 19:13:16 +0900

    Revert "ヘッダーにリンクを追加 (#17)"
    This reverts commit f24950b241841dee2b5710108ea9e73161a786b9.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,49 +1,34 @@
-import Image from "next/image";
-import Link from "next/link";
 import styles from "./Header.module.scss";
+import Image from "next/image";
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <Link href="/home/mypage" passHref legacyBehavior>
-        <h1 className={styles.logo}>SkinBloom</h1>
-      </Link>
+      <h1 className={styles.logo}>SkinBloom</h1>
       <div className={styles.rightAligned}>
         <nav className={styles.nav}>
           <ul className={styles.navList}>
-            <li key="Home" className={styles.navItem}>
-              <Link href="/home/mypage">Home</Link>
-            </li>
-            <li key="Search" className={styles.navItem}>
-              <Link href="/cosmetics/search">Search</Link>
-            </li>
-            <li key="Records" className={styles.navItem}>
-              <Link href="/">Records</Link>
-            </li>
-            {/* todo: あとでAPI接続する */}
-            <li key="Logout" className={styles.navItem}>
-              <Link href="/home/intro">Logout</Link>
-            </li>
+            {["Home", "Cosmetics", "Logs"].map((item, index) => (
+              <li key={index} className={styles.navItem}>
+                <a href={item.toLowerCase}>{item}</a>
+              </li>
+            ))}
           </ul>
         </nav>
-        <Link href="">
-          <Image
-            className={styles.icon}
-            alt=""
-            src="/icons/write.svg"
-            width={40}
-            height={40}
-          />
-        </Link>
-        <Link href="/setting/profile">
-          <Image
-            className={styles.icon}
-            alt=""
-            src="/icons/person.svg"
-            width={40}
-            height={40}
-          />
-        </Link>
+        <Image
+          className={styles.icon}
+          alt=""
+          src="/icons/write.svg"
+          width={40}
+          height={40}
+        />
+        <Image
+          className={styles.icon}
+          alt=""
+          src="/icons/person.svg"
+          width={40}
+          height={40}
+        />
       </div>
     </header>
   );
