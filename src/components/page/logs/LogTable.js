e6ac62abceb33edd4ff5d1b81f31a8a74584960b
ten10ba <59526400+ten10ba@users.noreply.github.com>
2024-03-19 20:29:44 +0900

    評価と概形完成

--- src/components/page/logs/LogTable.js
+++ src/components/page/logs/LogTable.js
@@ -1,36 +1,114 @@
 import styles from "./LogTable.module.scss";
 import Image from "next/image";
 import Link from "next/link";
+import React, { useState } from 'react';
+
+const Rating = ({ value }) => {
+    // 評価値に応じて星の数を生成
+    const stars = [];
+    for (let i = 0; i < 5; i++) {
+      stars.push(
+        <Image
+          key={i}
+          src={i < value ? '/icons/yellowstar.svg' : '/icons/graystar.svg'} // 星の色を評価値に基づいて設定
+          width={15}
+          height={15}
+        />
+      );
+    }
+    return <div className={styles.rating}>{stars}</div>;
+};
+  
 
 export default function LogTable() {
+  const data = [
+    {
+        id: 1,
+        productImage: "/imgs/chifure_all_in_one.jpg",
+        productName: "美白 うるおい ジェル",
+        productSubName: "ちふれ 美白 うるおい ジェル N",
+        category: "オールインワン",
+        evaluation: 3,
+        positive: ["うるおい"],
+        negative: ["ニキビに効果感じられず"],
+        date: "2021-10-01"
+    }
+  ];
   return (
     <table className={styles.logTable}>
         <thead>
             <tr>
-            <th></th>
-            <th>商品名</th>
-            <th>カテゴリ</th>
-            <th>評価</th>
-            <th>Tags</th>
-            <th>Tags</th>
-            <th>記録日</th>
+            <th className = {styles.logTableHeader}></th>
+            <th className = {styles.logTableHeader}></th>
+            <th className = {styles.logTableHeader}>
+                <div className={styles.productNameTableHeader}>
+                    商品名
+                </div>
+            </th>
+            <th className = {styles.logTableHeader}>
+                <div className={styles.categoryTableHeader}>
+                    カテゴリ
+                </div>
+            </th>
+            <th className = {styles.logTableHeader}>
+                <div className={styles.ratingTableHeader}>
+                    評価
+                </div>
+            </th>
+            <th className = {styles.logTableHeader}>
+                <div className={styles.positiveTableHeader}>
+                    ポジティブ
+                </div>
+            </th>
+            <th className = {styles.logTableHeader}>
+                <div className={styles.negativeTableHeader}>
+                    ネガティブ
+                </div>
+            </th>
+            <th className = {styles.logTableHeader}>
+                <div className={styles.dateTableHeader}>
+                    記録日
+                </div>
+            </th>
             </tr>
         </thead>
         <tbody>
-            <tr>
-            <td></td>
-            <td className={styles.imageCell}>
-                <div className={styles.imageContainer}>
-                    <Image alt="" src="../../../../public/imgs/chifure_all_in_one.jpg" width={60} height={60} />
-                </div>
-                美白 うるおい ジェル
+        {data.map((item, index) => (
+            <tr key={index}>
+            <td className={styles.cell}>
+                <label className={styles.checkbox}>
+                    <input type="checkbox" />
+                    <span />
+                </label>
             </td>
-            <td>オールインワン</td>
-            <td>ほし</td>
-            <td>うるおい</td>
-            <td>ニキビに効果感じられず</td>
-            <td>2024/03/17</td>
+            <td className={styles.cell}>
+                <div className={styles.imageContainer}>
+                    <Image alt="" src={item.productImage} width={60} height={60} />
+                </div>
+            </td>
+            <td className={styles.cell}>
+                <div className={styles.productName}>
+                <Link href={`/logs/${encodeURIComponent(item.id)}`} passHref>
+                    {item.productName}
+                </Link>
+                </div>
+                <div className={styles.productSubName}>
+                    {item.productSubName}
+                </div>
+            </td>
+            <td className={styles.cell}>{item.category}</td>
+            <td className={styles.cell}>
+                <Rating value={item.evaluation} />
+            </td>
+            <td className={styles.cell}>{item.positive.join(', ')}</td>
+            <td className={styles.cell}>{item.negative.join(', ')}</td>
+            <td className={styles.cell}>
+                <div className={styles.date}>
+                    {item.date}
+                </div>
+            </td>
             </tr>
+        ))}
         </tbody>
     </table>
   );
