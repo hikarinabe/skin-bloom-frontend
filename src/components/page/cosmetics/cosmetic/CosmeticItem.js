c488c6fd5c581586b984d23e03d1fdf186145454
Namiko <namiko.trym@gmail.com>
2024-03-20 01:45:40 +0900

    商品IDのページを作成+API実装 (#23)
    * 横並びまで実装
    
    * id pageの実装
    
    * debug部分の削除
    
    * run lint
    
    * add mergine

---
+++ src/components/page/cosmetics/cosmetic/CosmeticItem.js
@@ -0,0 +1,135 @@
+import tagStyles from "@/styles/button/SoftEdgeTagButton.module.scss";
+import Grid from "@mui/material/Grid";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import styles from "./CosmeticItem.module.scss";
+
+export default function CosmeticItems() {
+  const router = useRouter();
+  const { id } = router.query;
+  const [response, setResponse] = useState({
+    name: "",
+    price: -1,
+    company: -1,
+    category: 0,
+    ingredients: [],
+  });
+  const company_data = { 120: "ちふれ" };
+  const category_data = {
+    1: "化粧水",
+    2: "乳液",
+    3: "美容液",
+    4: "洗顔フォーム",
+    5: "クレンジング",
+    6: "クリーム",
+    7: "オールインワン",
+    8: "パック・シートマスク",
+  };
+  const matchRate = 0.8;
+  const majorTags = ["保湿"];
+
+  const getClients = async () => {
+    console.log(id);
+    const endpoint_url = `https://asia-northeast1-hikarinabe-741d2.cloudfunctions.net/cosmetic_info?cosmetic_id=${id}`;
+    const requestOptions = {
+      method: "GET",
+      headers: { Authorization: "wJ5C9dFcEMB5" },
+    };
+
+    try {
+      const res = await fetch(endpoint_url, requestOptions);
+      const data = await res.text();
+      const json_data = JSON.parse(data);
+
+      console.log(json_data);
+      setResponse(() => ({
+        name: json_data["name"],
+        price: json_data["price"],
+        company: company_data[json_data["company"]],
+        category: category_data[json_data["category"]],
+        ingredients: json_data["ingredients"],
+      }));
+      console.log(response.name);
+    } catch (err) {
+      alert("エラーが発生しました");
+    }
+  };
+
+  useEffect(() => {
+    console.log(router.isReady);
+    if (router.isReady) {
+      getClients();
+    }
+  }, [router]);
+  return (
+    <div className={styles.cosmeticItemWrapper}>
+      <div className={styles.contents}>
+        <p className={styles.title}>{response.name}</p>
+
+        <Grid container spacing={1}>
+          <Grid item xs={4} sm={6}>
+            <Image
+              src={`/item_imgs/${id}.jpg`}
+              height={300}
+              width={300}
+              alt=""
+              className={styles.cosmeticImage}
+            />
+          </Grid>
+          <Grid item xs={7} sm={6}>
+            <Grid container spacing={2}>
+              <Grid item xs={6}>
+                <div className={styles.categoryIcon}>{response.category}</div>
+              </Grid>
+              <Grid item xs={4}>
+                <div className={styles.matchRateWrapper}>
+                  <Image src="/icons/heart.svg" height={25} width={25} alt="" />
+                  <div>{matchRate * 100}%</div>
+                </div>
+              </Grid>
+            </Grid>
+            <div className={styles.itemInfo}>
+              <div>
+                {response.company} {response.price} 円 (税込)
+              </div>
+            </div>
+            <div className={styles.tagsWrapper}>
+              <p>タグ:</p>
+              {majorTags.map((value) => (
+                <button
+                  className={tagStyles.softEdgeTagButtonActive}
+                  key={value}
+                >
+                  {value}
+                </button>
+              ))}
+            </div>
+            <button className={styles.pillShapedButtonBlue}>
+              この商品から記録を作成
+            </button>
+          </Grid>
+        </Grid>
+
+        <div>
+          <p className={styles.center}>成分</p>
+          <ListItem component="div">
+            <ul>
+              {response.ingredients.map((ingredient, index) => (
+                <ListItemButton key={index}>
+                  <ListItemText
+                    primary={ingredient}
+                    className={styles.ingredientList}
+                  />
+                </ListItemButton>
+              ))}
+            </ul>
+          </ListItem>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/cosmetics/[id].js
+++ src/pages/cosmetics/[id].js
@@ -1,3 +1,12 @@
+import Layout from "@/components/Layout/Layout";
+import CosmeticItems from "@/components/page/cosmetics/cosmetic/CosmeticItem";
+
 export default function Detail() {
-  return <></>;
+  return (
+    <Layout>
+      <main>
+        <CosmeticItems />
+      </main>
+    </Layout>
+  );
 }
