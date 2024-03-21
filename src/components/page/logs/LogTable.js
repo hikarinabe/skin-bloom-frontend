0d91576b52cb3611d52e2f627ea9a06d4e43d04c
ss-cosmos-hue <sayo34hashimoto@gmail.com>
2024-03-22 00:39:17 +0900

    update ui

--- src/components/page/logs/LogTable.js
+++ src/components/page/logs/LogTable.js
@@ -86,104 +86,109 @@ export default function LogTable() {
   };
 
   return (
-    <table className={styles.logTable}>
-      <thead>
-        <tr>
-          <th className={styles.logTableHeader}></th>
-          <th className={styles.logTableHeader}>
-            <div className={styles.productNameTableHeader}>商品名</div>
-          </th>
-          <th className={styles.logTableHeader}>
-            <div className={styles.categoryTableHeader}>カテゴリ</div>
-          </th>
-          <th
-            className={styles.logTableHeader}
-            onClick={() => requestSort("rating")}
-          >
-            <div className={styles.ratingTableHeader}>評価</div>
-          </th>
-          <th className={styles.logTableHeader}>
-            <div className={styles.positiveTableHeader}>ポジティブ</div>
-          </th>
-          <th className={styles.logTableHeader}>
-            <div className={styles.negativeTableHeader}>ネガティブ</div>
-          </th>
-          <th className={styles.logTableHeader}>
-            <div
-              className={styles.dateTableHeader}
-              onClick={() => requestSort("date")}
+    <div className={styles.logTableWrapper}>
+      <table className={styles.logTable}>
+        <thead>
+          <tr>
+            <th className={styles.logTableHeader}></th>
+            <th className={styles.logTableHeader}>
+              <div className={styles.productNameTableHeader}>商品名</div>
+            </th>
+            <th className={styles.logTableHeader}>
+              <div className={styles.categoryTableHeader}>カテゴリ</div>
+            </th>
+            <th
+              className={styles.logTableHeader}
+              onClick={() => requestSort("rating")}
             >
-              記録日
-            </div>
-          </th>
-          <th className={styles.logTableHeader}>
-            <div
-              className={styles.commentTableHeader}
-              onClick={() => requestSort("date")}
-            >
-              コメント
-            </div>
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((item, index) => (
-          <tr key={index}>
-            <td className={styles.cell}>
-              <div className={styles.imageContainer}>
-                <Image
-                  alt=""
-                  src={`/item_imgs/${item.id}.jpg`}
-                  width={60}
-                  height={60}
-                />
+              <div className={styles.ratingTableHeader}>評価</div>
+            </th>
+            <th className={styles.logTableHeader}>
+              <div className={styles.positiveTableHeader}>ポジティブ</div>
+            </th>
+            <th className={styles.logTableHeader}>
+              <div className={styles.negativeTableHeader}>ネガティブ</div>
+            </th>
+            <th className={styles.logTableHeader}>
+              <div
+                className={styles.dateTableHeader}
+                onClick={() => requestSort("date")}
+              >
+                記録日
               </div>
-            </td>
-            <td className={styles.cell}>
-              <div className={styles.name}>
-                <Link href={`/cosmetics/${item.id}`} passHref>
-                  {item.item_name}
-                </Link>
+            </th>
+            <th className={styles.logTableHeader}>
+              <div
+                className={styles.commentTableHeader}
+                onClick={() => requestSort("date")}
+              >
+                コメント
               </div>
-              <div className={styles.name}>{item.item_name}</div>
-            </td>
-            <td className={styles.cell}>{to_str_category(item.category)}</td>
-            <td className={styles.cell}>
-              <Rating value={item.rate} />
-            </td>
-            <td className={styles.cell}>
-              <div className={styles.tagContainer}>
-                {item.good_tag.map((value) => (
-                  <button
-                    className={tagStyles.softEdgeTagButtonActive}
-                    key={value}
-                  >
-                    {tag_list[value - 1].name}
-                  </button>
-                ))}
-              </div>
-            </td>
-            <td className={styles.cell}>
-              <div className={styles.tagContainer}>
-                {item.bad_tag.map((value) => (
-                  <button
-                    className={tagStyles.softEdgeTagButtonWhite}
-                    key={value}
-                  >
-                    {tag_list[value - 1].name}
-                  </button>
-                ))}
-              </div>
-            </td>
-            <td className={styles.cell}>
-              <div className={styles.date}>{item.date}</div>
-            </td>
-            <td className={styles.cell}>
-              <div className={styles.comment}>{item.comment}</div>
-            </td>
+            </th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index}>
+              <td className={styles.cell}>
+                <div className={styles.imageContainer}>
+                  <Image
+                    alt=""
+                    src={`/item_imgs/${item.id}.jpg`}
+                    width={60}
+                    height={60}
+                  />
+                </div>
+              </td>
+              <td className={styles.cell}>
+                <div className={styles.name}>
+                  <Link
+                    href={`/cosmetics/${item.id}`}
+                    passHref
+                    className={styles.noLinkStyle}
+                  >
+                    {item.item_name}
+                  </Link>
+                </div>
+              </td>
+              <td className={styles.cell}>{to_str_category(item.category)}</td>
+              <td className={styles.cell}>
+                <Rating value={item.rate} />
+              </td>
+              <td className={styles.cell}>
+                <div className={styles.tagContainer}>
+                  {item.good_tag.map((value) => (
+                    <button
+                      className={tagStyles.softEdgeTagButtonActive}
+                      key={value}
+                    >
+                      {tag_list[value - 1].name}
+                    </button>
+                  ))}
+                </div>
+              </td>
+              <td className={styles.cell}>
+                <div className={styles.tagContainer}>
+                  {item.bad_tag.map((value) => (
+                    <button
+                      className={tagStyles.softEdgeTagButtonWhite}
+                      key={value}
+                    >
+                      {tag_list[value - 1].name}
+                    </button>
+                  ))}
+                </div>
+              </td>
+              <td className={styles.cell}>
+                <div className={styles.date}>{item.date.replace("T", " ")}</div>
+              </td>
+              <td className={styles.cell}>
+                <div className={styles.comment}>{item.comment}</div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
