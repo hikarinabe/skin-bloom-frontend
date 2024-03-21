52ff747090b1056639bb05bc602739ec5fa6ebd0
ss-cosmos-hue <sayo34hashimoto@gmail.com>
2024-03-21 20:32:58 +0900

    run formatter

--- src/pages/setting/index.js
+++ src/pages/setting/index.js
@@ -5,7 +5,8 @@ export default function Setting() {
   const router = useRouter();
 
   useEffect(() => {
-    const isLoggedIn = typeof localStorage !== "undefined" && localStorage.getItem("user_id");
+    const isLoggedIn =
+      typeof localStorage !== "undefined" && localStorage.getItem("user_id");
 
     if (isLoggedIn) {
       router.push("/setting/profile");
