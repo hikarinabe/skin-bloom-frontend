533f296be6e6f296b23a714ecd496f0d6ffd113c
Namiko <namiko.trym@gmail.com>
2024-03-26 17:35:02 +0900

    home-mypage スマホ用に修正 (#72)
    * cosmetic cardの修正
    
    * 画面の配置を変更
    
    * home-mypageの修正
    
    * flexをmdに
    
    * matchRate wrapperを戻した

--- src/components/CosmeticCard/CosmeticCard.js
+++ src/components/CosmeticCard/CosmeticCard.js
@@ -1,13 +1,11 @@
 import { to_str_category, to_str_company } from "@/pkg/cosmetic_master";
-import styles from "./CosmeticCard.module.scss";
-
-import tagStyles from "@/styles/tag/tag.module.scss";
-
 import { tag_list } from "@/pkg/tag";
+import tagStyles from "@/styles/tag/tag.module.scss";
 import Image from "next/image";
 import Link from "next/link";
-
+import "tailwindcss/tailwind.css";
 import StarRatingDisplay from "../StarRating/StarRatingDisplay";
+import styles from "./CosmeticCard.module.scss";
 
 export default function CosmeticCard({ cosmetic, isMyPage = true }) {
   const cosmetic_id = cosmetic.id;
@@ -75,7 +73,7 @@ export default function CosmeticCard({ cosmetic, isMyPage = true }) {
   );
 }
 
-export function CosmeticLogCard({ cosmetic, isMyPage = true }) {
+export function CosmeticLogCard({ cosmetic }) {
   const cosmetic_id = cosmetic.id;
   const imgsrc = `/item_imgs/${cosmetic_id}.jpg`;
   const name = cosmetic.item_name;
@@ -84,56 +82,37 @@ export function CosmeticLogCard({ cosmetic, isMyPage = true }) {
   const likeRate = cosmetic.rate;
 
   return (
-    <div className={styles.cosmeticCard}>
+    <div className={`${styles.cosmeticCard} relative`}>
       <div className={styles.cosmeticImageWrapper}>
         <Image src={imgsrc} height={250} width={250} alt="" />
       </div>
       <p>{categoryName}</p>
       <h3>{name}</h3>
-      <div className={styles.horizontalWrapper}>
-        <div></div>
-        {isMyPage ? (
-          <div className={styles.matchRateWrapper}>
-            <StarRatingDisplay value={likeRate} />
-          </div>
-        ) : null}
+      <div className="flex flex-row-reverse ">
+        <div>
+          <StarRatingDisplay value={likeRate} />
+        </div>
       </div>
       <hr className={styles.division}></hr>
+
       <div className={styles.horizontalWrapper}>
         <div className={styles.tagsWrapper}>
           {good_tag.map((value) => (
-            <button className={tagStyles.tagActive} key={value}>
-              {tag_list[value - 1].name} に効果
+            <button className={`${tagStyles.tagActive} mt-1 mr-1`} key={value}>
+              ◎ {tag_list[value - 1].name}
             </button>
           ))}
         </div>
       </div>
 
-      <div className={`${styles.horizontalWrapper} ${styles.marginTop}`}>
-        <Link
-          href={`/cosmetics/${cosmetic_id}`}
-          className={styles.detailButtonWrapper}
-        >
-          <button className={styles.detailButton}>
-            <h3 className={styles.detailButtonText}>詳しく見る➚</h3>
-          </button>
-        </Link>
-
-        {isMyPage ? null : (
-          <Link
-            href={{
-              pathname: "/logs/new",
-              query: { cosmetic_id: cosmetic_id },
-            }}
-          >
-            <Image
-              src="/icons/circular-write.svg"
-              height={25}
-              width={25}
-              alt=""
-            />
+      <div className={`flex justify-center mt-2`}>
+        <div className="w-4/6 absolute bottom-3">
+          <Link href={`/cosmetics/${cosmetic_id}`}>
+            <button className={styles.detailButton}>
+              <h3 className={styles.detailButtonText}>詳しく見る➚</h3>
+            </button>
           </Link>
-        )}
+        </div>
       </div>
     </div>
   );
