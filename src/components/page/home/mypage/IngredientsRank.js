e4a0a773d433cb2d513f6f38e5e41d9007a94946
Gre3nT <fabulous648@gmail.com>
2024-03-27 01:17:24 +0900

    ランキングの位置調整

--- src/components/page/home/mypage/IngredientsRank.js
+++ src/components/page/home/mypage/IngredientsRank.js
@@ -7,28 +7,30 @@ export default function IngredientsRank({
   ingredients = ["グリセリン", "水", "砂糖"],
 }) {
   return (
-    <div className={styles.ingredientsRank}>
-      <div className={styles.ingredientsRankTitle}>
-        <Image
-          src={`/icons/${right ? "happy" : "sad"}.svg`}
-          height={25}
-          width={25}
-          alt=""
-        />
-        <p>あなたに{right ? "合う" : "合わない"}成分</p>
-      </div>
-      <div className={styles.ingredientsRankItemsWrapper}>
-        {ingredients.map((value, index) => (
-          <div className={styles.ingredientRankItem} key={index}>
-            <Image
-              src={`/icons/rank${index + 1}.svg`}
-              height={32}
-              width={41}
-              alt=""
-            />
-            <p>{value}</p>
-          </div>
-        ))}
+    <div className="md:w-96">
+      <div className={styles.ingredientsRank}>
+        <div className={styles.ingredientsRankTitle}>
+          <Image
+            src={`/icons/${right ? "happy" : "sad"}.svg`}
+            height={25}
+            width={25}
+            alt=""
+          />
+          <p>あなたに{right ? "合う" : "合わない"}成分</p>
+        </div>
+        <div className={styles.ingredientsRankItemsWrapper}>
+          {ingredients.map((value, index) => (
+            <div className={styles.ingredientRankItem} key={index}>
+              <Image
+                src={`/icons/rank${index + 1}.svg`}
+                height={32}
+                width={41}
+                alt=""
+              />
+              <p>{value}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
