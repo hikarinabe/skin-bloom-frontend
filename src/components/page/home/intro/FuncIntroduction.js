1c21840c93baccf084f4fa072a782d90f2f6f704
ten10ba <59526400+ten10ba@users.noreply.github.com>
2024-03-29 01:18:26 +0900

    npx prettier  --write src/

--- src/components/page/home/intro/FuncIntroduction.js
+++ src/components/page/home/intro/FuncIntroduction.js
@@ -1,6 +1,5 @@
 import styles from "./FuncIntroduction.module.scss";
 
-
 import Image from "next/image";
 
 export default function FuncIntroduction() {
@@ -13,7 +12,7 @@ export default function FuncIntroduction() {
     <div className={styles.sectionWrapper}>
       <p className={styles.subtitle}>機能紹介</p>
       {/* <div class="flex flex-col md:flex-row gap-8"> */}
-        {/* <div className={styles.funcItem}>
+      {/* <div className={styles.funcItem}>
           <Image src="/imgs/subscription.jpg" alt="" width={400} height={300} />
           <div className={styles.caption}>
             <p className={styles.captionText}>サブスクで化粧品を試す</p>
@@ -32,19 +31,28 @@ export default function FuncIntroduction() {
             <p className={styles.captionText}>自分の肌に合う化粧品を知る</p>
           </div>
         </div> */}
-        <div className="flex flex-col md:flex-row gap-8 justify-center items-center mx-auto" style={{ width: '90%' }}>
-          {items.map((item, index) => (
-            <div key={index} className="flex flex-col basis-1/3 max-w-xs md:max-w-sm lg:max-w-md xl:max-w-lg mx-auto">
-              <div className="bg-gray-200 h-48 bg-no-repeat bg-center bg-cover rounded-t-lg" style={{ backgroundImage: `url(${item.src})` }}>
-                {/* Image as background */}
-              </div>
-              <div className="p-4 bg-white rounded-b-lg">
-                <p className="text-center text-gray-600">{item.caption}</p>
-              </div>
+      <div
+        className="flex flex-col md:flex-row gap-8 justify-center items-center mx-auto"
+        style={{ width: "90%" }}
+      >
+        {items.map((item, index) => (
+          <div
+            key={index}
+            className="flex flex-col basis-1/3 max-w-xs md:max-w-sm lg:max-w-md xl:max-w-lg mx-auto"
+          >
+            <div
+              className="bg-gray-200 h-48 bg-no-repeat bg-center bg-cover rounded-t-lg"
+              style={{ backgroundImage: `url(${item.src})` }}
+            >
+              {/* Image as background */}
             </div>
-          ))}
-        </div>
-        {/* </div> */}
+            <div className="p-4 bg-white rounded-b-lg">
+              <p className="text-center text-gray-600">{item.caption}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      {/* </div> */}
     </div>
   );
 }
