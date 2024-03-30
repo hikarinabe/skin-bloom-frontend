5ebdb4476270015bb4bcdbc09a4b812ea6f81faa
NamikoToriyama <torichan@mercari.com>
2024-03-30 17:38:07 +0900

    add empty env.js

---
+++ src/env.js
@@ -0,0 +1,12 @@
+// API keyやエンドポイント周りのkeyを管理するコード
+// https://github.com/hikarinabe/env/blob/main/env.js
+export const API_KEY = "";
+
+export const LOGIN_ENDPOINT = "";
+export const USER_ENDPOINT = "";
+export const AUTH_ENDPOINT = "";
+export const COSMETIC_INFO_ENDPOINT = "";
+export const HOME_ENDPOINT = "";
+export const COSMETIC_LIST_ENDPOINT = "";
+export const COSMETIC_LOG_ENDPOINT = "";
+// export const _ENDPOINT = ;
